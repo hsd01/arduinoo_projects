3db00b21bf85c1dc9910c9f2c2916ebda8f13f5c
hsd01 <ashuhemantsingh@gmail.com>
2023-12-02 17:18:58 +0530

    update main

--- Hemant_raspberry_pi_arduino_esp/main.py.py
+++ Hemant_raspberry_pi_arduino_esp/main.py.py
@@ -9,6 +9,7 @@ import random
 eng = pyttsx3.init()
 eng.setProperty('voice', 'en-us')
 eng.setProperty('pitch', 0.8)
+voices = eng.getProperty('voices')
 eng.setProperty('voice', voices[1].id)
 
 bus = smbus.SMBus(1)
@@ -36,19 +37,19 @@ def greet():
     time_now = cu_time[0]
     rand = random.randint(1,(len(hello)-1))
     if int(time_now) >=0 and int(time_now) < 12:
-        en.say(hello[rand]+" Good morning")
-        en.runAndWait()
+        eng.say(hello[rand]+" Good morning")
+        eng.runAndWait()
     elif int(time_now) >=12 and int(time_now) < 22:
-        en.say(hello[rand]+" good afternoon ")
-        en.runAndWait()
+        eng.say(hello[rand]+" good afternoon ")
+        eng.runAndWait()
     else:
-        en.say(hello[rand]+"its already bead time good night")
-        en.runAndWait()
-en.say("Hello My name is Zenny")
-en.runAndWait()
-client_soc, address = server_soc.accept()
-greet()
-print("Accepted connection from {} ".format(address))
+        eng.say(hello[rand]+"its already bead time good night")
+        eng.runAndWait()
+#eng.say("Hello My name is Zenny")
+#eng.runAndWait()
+#client_soc, address = server_soc.accept()
+#greet()
+#print("Accepted connection from {} ".format(address))
 
 def main():
     while True:
@@ -58,37 +59,44 @@ def main():
         dis = bus.read_byte(i2c_address)
         if data == b"exit":
             client_soc.send("Stoping by android app:")
-            en.say("stoped from mobile app exiting")
-            en.runAndWait()
+            eng.say("stoped from mobile app exiting")
+            eng.runAndWait()
             print("Exit:")
             break
         elif data == b"move forward" or data == b"forward":
             print("Forward march")
-            en.say("Moving forward")
-            en.runAndWait()
+            eng.say("Moving forward")
+            eng.runAndWait()
             bus.write_byte(i2c_address,1)
         elif data == b"move backward" or data == b"backward":
             print("Reverse")
-            en.say("Moving reverse")
-            en.runAndWait()
+            eng.say("Moving reverse")
+            eng.runAndWait()
             bus.write_byte(i2c_address,2)
         elif data == b"turn left" or data == b"left":
             print("left")
-            en.say("Moving left")
-            en.runAndWait()
+            eng.say("Moving left")
+            eng.runAndWait()
             bus.write_byte(i2c_address,3)
         elif data == b"turn right" or data == b"right":
             print("turn right")
-            en.say("Moving right")
-            en.runAndWait()
+            eng.say("Moving right")
+            eng.runAndWait()
             bus.write_byte(i2c_address,4)
         elif data == b"stop" or data == b"halt":
             print("stop")
-            en.say("stoping")
-            en.runAndWait()
+            eng.say("stoping")
+            eng.runAndWait()
             bus.write_byte(i2c_address,5)
         if dis <=10 :
             print("stop")
             bus.write_byte(i2c_address,5)
     client_soc.close()
     server_soc.close()
+    
+eng.say("Hello My name is Zenny")
+eng.runAndWait()
+client_soc, address = server_soc.accept()
+greet()
+print("Accepted connection from {} ".format(address))
+main()
